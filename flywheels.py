c149b798f3a8dbe6ed9823e770c287a5116a9cf3
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2026-02-18 00:54:48 +0700

    Update flywheels.py

--- flywheels.py
+++ flywheels.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-from scipy.stats import norm 
+from scipy.stats import norm
 import json
 import os
 import re
